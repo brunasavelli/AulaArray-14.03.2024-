8c5c130eabdd431335477f9ae092d9e3f43a3f8b
Bruna Savelli <bnsavelli@gmail.com>
2024-03-14 17:16:06 -0300

    Exemplo .pop

---
+++ pop.js
@@ -0,0 +1,11 @@
+//Método .pop
+let listaConvidados = ['Ana', 'João', 'Maria', 'Pedro'];
+
+console.log(listaConvidados);
+
+let ultimoConvidado = listaConvidados.pop();
+
+console.log(ultimoConvidado);
+console.log(listaConvidados);
+
+//Esse método remove o último elemento do array e o retorna. 
