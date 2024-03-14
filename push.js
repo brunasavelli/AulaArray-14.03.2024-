df6ab7f519a329bc8ef4b7a3df009cc159dc9dd8
Bruna Savelli <bnsavelli@gmail.com>
2024-03-14 17:09:40 -0300

    Exemplo push

---
+++ push.js
@@ -0,0 +1,11 @@
+//Método .push
+
+let numeros = [1, 2, 3, 4, 5];
+numeros.push(6);
+
+console.log(numeros);
+numeros.push(7, 8, 9, 10);
+
+console.log(numeros);
+
+//O método .push adciona um ou mais elementos no final de um array e retorna o novo comprimento desse array.
